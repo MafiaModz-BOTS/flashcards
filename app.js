6986c565e6c6efaf3d63e23b8054c4a72464d2fb
Samuel <105346833+MafiaModz-BOTS@users.noreply.github.com>
2025-08-13 11:09:55 +0000

    pergunta

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const cartao = document.createElement(article)
 cartao.className = 'cartao'
 
 cartao.innerHTML = `
+        <div class="cartao_conteudo">
+            <h3>${categoria}</h3>
+            <div class="pergunta">
+                ${pergunta}
+            </div>
+            <div class="resposta">
+                ${resposta}
+            </div>
+        </div>
+    `
+
+cartao.innerHTML = `
 <div class="cartao_conteudo"`
 
 }
